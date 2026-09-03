57b8abc50f7a2e9bd2c076fe8dff5f6a6f3a8266
nandoedwin2213 <nandoedwin2213@github.com>
2026-09-03 15:09:12 -0500

    Correct ESMA squadrons: Escorpiones (DA-20 Katana) and Linces (Grob G-120TP)

--- src/components/RegistrationStep.tsx
+++ src/components/RegistrationStep.tsx
@@ -40,8 +40,8 @@ const FAE_REPARTOS: RepartoOption[] = [
     name: 'Escuela Superior de Aviación "Cosme Rennella" (ESMA)',
     location: 'Salinas',
     squadrons: [
-      'Escuadrón "Halcones" (Aeronaves DA-20 Katana)',
-      'Escuadrón "Escorpiones" (Aeronaves Grob G-120TP)',
+      'Escuadrón "Escorpiones" (Aeronaves DA-20 Katana)',
+      'Escuadrón "Linces" (Aeronaves Grob G-120TP)',
       'Escuadrón Cadetes / Vuelo Inicial',
       'Escuadrón Entrenador Primario',
     ],
